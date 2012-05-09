c6fba35d1b39f3b5261474379545c419feec4bbd
SanoZ <sandrine34bis@gmail.com>
2012-05-09 22:00:41 +1000

    first init

--- application/controllers/ApplicationController.php
+++ application/controllers/ApplicationController.php
@@ -1,18 +1,74 @@
 <?php
 
-class ApplicationController extends Zend_Controller_Action
+abstract class ApplicationController extends Zend_Controller_Action
 {
-
-    public function init()
+    protected $_checkActionCookies = true;
+    protected $_loggedUser;
+    
+    /**
+     * @return Zend_Cache
+     */
+    static public function getCache($type = 'general')
     {
-        echo "hahaha";/* Initialize action controller here */
+        $_manager = new Zend_Cache_Manager();
+        $_cache = $_manager->getCache($type);
+        if (empty($_cache)) {
+            
+            // Can be switched to memcached later
+            if ('general' == $type) {
+                $_cache = Zend_Cache::factory('Core', 'File', array(
+                        'lifetime'                  => 60 * 5,
+                        'automatic_serialization'   => true
+                    ), array(
+                        'cache_dir'                 => '/tmp'
+                    )
+                );
+                
+            // Strict for file cache
+            } elseif ('file' == $type) {
+                $_cache = Zend_Cache::factory('Core', 'File', array(
+                        'lifetime'                  => 60 * 5,
+                        'automatic_serialization'   => true
+                    ), array(
+                        'cache_dir'                 => '/tmp'
+                    )
+                );
+            }
+            
+            $_manager->setCache($type, $_cache);
+        }
+        return $_cache;
     }
-
-    public function indexAction()
+   
+    
+    static public function getTestCookieName($testName)
     {
-        // action body
+        return 't_' . substr(md5($testName), 0, 5);
     }
-
-
+    
+    public function preDispatch()
+    {
+        parent::preDispatch();
+        
+        Zend_Db_Table_Abstract::setDefaultMetadataCache(self::getCache());
+        
+        if (!defined('__RELEASE_HASH')) {
+            define('__RELEASE_HASH', substr(md5('ollio'), 0, 10));
+        }
+        
+        if (!defined('__SITE_URL')) {
+            define('__SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+        }
+        
+        $_userModel = new Model_User();
+        if (true === $_userModel->getCurrentUser()) {
+            $this->view->loggedUser = $this->_loggedUser = $_userModel;
+        }
+        
+        if (true === $this->_checkActionCookies) {
+            $this->_parseActionCookies();
+        }
+    }
+    
+    
 }
-
